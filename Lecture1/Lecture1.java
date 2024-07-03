271c83e30e774c4b2579086eaf2efa220f47dfba
nightguarder <73370044+nightguarder@users.noreply.github.com>
2024-07-03 17:03:34 +0200

    Prednaska 1 - základy completed

---
+++ Lecture1/Lecture1.java
@@ -0,0 +1,31 @@
+package Lecture1;
+public class Lecture1 {
+    //By making Bytecode a static nested class, 
+    //you can instantiate it directly using new Bytecode()
+    public  static class Bytecode { //Stack=2, Heap=1, Locals=3, Args_size=2
+        public int plus (int a){    //0:  iconst_1
+        int b = 1;                  //1:  istore_2
+        return a + b;               //2:  iload_1 + iload_2 + iadd
+        }                           //3:  ireturn
+    }
+    //By making Bytecode1 a non-static nested class, you need an outer class (Lecture 1) 
+    //to create an instance to instantiate it 
+    public class Bytecode1 {    
+        public int plus (int a){
+        int b = 2;
+        return a + b;
+        }
+        
+    }
+    //Main method
+    public static void main(String[] args) {
+        System.out.println("Bytecode interpretation:");
+        System.out.println("plus(1) = " + new Bytecode().plus(1));
+        System.out.println("plus(2) = " + new Lecture1().new Bytecode1().plus(2));
+    }
+}
+//Single line comment
+/* Multi line comment 
+    Another line 
+*/
+// 📌 Lecture 1 📝 🗑️
